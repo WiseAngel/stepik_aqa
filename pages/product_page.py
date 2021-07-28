d18c081a68e25a9232bfee7c41b112bb18868f3d
WiseAngel <wiseangelgames@gmail.com>
2021-07-28 11:56:50 +0300

    4.2.3 converted to class

--- pages/product_page.py
+++ pages/product_page.py
@@ -29,10 +29,10 @@ class ProductPage(BasePage):
     def added_selected_book(self):
 
         self.book_in_basket = self.browser.find_element(*PPL.MESSAGE_WITH_BOOK_TITLE).text
-        assert self.book_title == self.book_in_basket, f'Вы добавили в корзину {self.book_title}, а в корзине оказалась {self.book_in_basket}'
+        assert self.book_title == self.book_in_basket, f'Добавили в корзину {self.book_title}, а в корзине оказалась {self.book_in_basket}'
 
     
     def price_corresponds_to_the_book(self):
 
         self.сost_book_in_basket = self.browser.find_element(*PPL.MESSAGE_WITH_PRICE).text
-        assert self.book_price == self.сost_book_in_basket, f'Стоимость выбранной книги {self.book_price}, цена книги в корзине {self.сost_book_in_basket}'
+        assert self.book_price == self.сost_book_in_basket, f'Стоимость выбранной книги {self.book_price}, стоимость книги в корзине {self.сost_book_in_basket}'
--- test_product_page.py
+++ test_product_page.py
@@ -1,11 +1,18 @@
+from typing import List
+import pytest
+
 from pages.product_page import ProductPage
 
 
-def test_guest_can_add_product_to_basket(browser):
+class TestProductPage:
 
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    product_page = ProductPage(browser, link) 
-    product_page.open()   
-    product_page.add_to_cart()
-    product_page.solve_quiz_and_get_code()
-    product_page.check_added_book()
+    links = ["http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"]
+
+    @pytest.mark.parametrize('link', links)
+    def test_guest_can_add_product_to_basket(self, browser, link: str):
+
+        product_page = ProductPage(browser, link) 
+        product_page.open()   
+        product_page.add_to_cart()
+        product_page.solve_quiz_and_get_code()
+        product_page.check_added_book()
